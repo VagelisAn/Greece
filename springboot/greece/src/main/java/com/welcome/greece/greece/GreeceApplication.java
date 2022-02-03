0869b8961b15f82b08c3838634968e9f7cfd40e0
VagelisAn <VagosM92@gmail.com>
2022-02-03 22:50:09 +0200

    Init

---
+++ springboot/greece/src/main/java/com/welcome/greece/greece/GreeceApplication.java
@@ -0,0 +1,13 @@
+package com.welcome.greece.greece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreeceApplication.class, args);
+	}
+
+}
---
+++ springboot/greece/src/test/java/com/welcome/greece/greece/GreeceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welcome.greece.greece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
